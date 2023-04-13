c1fb794b10a27b8d855bf06f4e43aa9505eef76d
adwitafernanda <adwitafernanda4@gmail.com>
2023-04-13 10:03:24 +0700

    Add project files.

---
+++ linearsearch/linearsearch.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int arr[20]; //array to be searched
+int n; //number of element in the array 
+int i; //Index of array element
+
+
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of element in the array: ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 element.\n\n";
+	}
+
+	//accept array
+}
